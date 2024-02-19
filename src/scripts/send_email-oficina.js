de8d2f7eb69d55529b8ee44caf993ba2a6013395
cristianUS <c.u.s21193@gmail.com>
2024-02-19 12:32:50 -0500

    firts launch

---
+++ src/scripts/send_email-oficina.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form-oficina');
+
+async function handleSendEmail (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    const response = await fetch("https://formspree.io/f/mjvnwjlq", {
+        method: "POST",
+        body: formData,
+        headers: {
+            Accept: "application/json"
+        }
+    });
+
+    if(response.ok) {
+        this.reset();
+        alert('Mensaje enviado.')
+    } else {
+        alert('Error al enviar el mensaje.')
+    }
+}
+
+form.addEventListener('submit', handleSendEmail);
